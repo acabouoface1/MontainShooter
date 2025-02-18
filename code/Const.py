1f59c8dbe4d250ced901a144dcd4e4ee9f914733
ThiagoPietro <tthiago.sa@hotmail.com>
2025-02-18 12:18:25 -0300

    Create asset folder Inserting all assets Creating the menu: background, sounds and fonts  Creating the font file

--- code/Const.py
+++ code/Const.py
@@ -1,2 +1,17 @@
+# C
+COLR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COOPERATIVE',
+               'SCORE',
+               'EXIT')
+
+
+
+
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
