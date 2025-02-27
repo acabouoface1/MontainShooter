085ea48c5e2faa9add5d972f131e49ef24d13e03
ThiagoPietro <tthiago.sa@hotmail.com>
2025-02-27 18:12:24 -0300

    SQLite3 implementation for Score using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -116,7 +116,7 @@ ENTITY_SHOOT_DELAY = {
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
-               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
 
@@ -142,3 +142,20 @@ TIMEOUT_LEVEL = 20000 # 20segundos
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH/ 2, 50),
+             'EnterName': (WIN_WIDTH/ 2, 80),
+             'Label': (WIN_WIDTH/ 2, 90),
+             'Name': (WIN_WIDTH/ 2, 110),
+             0: (WIN_WIDTH/ 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
